41805e9c58dbb90d4b20ad439d2687705f07ee5c
talha kayani <talha.kayani@invozone.com>
2021-08-24 13:34:12 +0500

    ES5 and ES6 Variable syntax

---
+++ Day 1/nodesyntac.js
@@ -0,0 +1,9 @@
+//ES5 Syntax
+var nameEs5 = "talha kayani es5";
+console.log(nameEs5);
+
+let nameEs6 = "talha kayani es6";
+console.log(nameEs6);
+
+const nameConstEs6 = "talha kayani const es6";
+console.log(nameConstEs6);
